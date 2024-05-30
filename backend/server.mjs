b1c311d4f77ff81c1d6a2147d33b6bd47e1b4649
malachi-crain <malachicrain@gmail.com>
2024-05-30 19:52:56 -0400

    Plan B for server pinging

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,7 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import https from 'https';
-import ckey from 'ckey';
 
 import contactController from './controllers/contactController.mjs';
 
@@ -20,32 +18,3 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-const pingServer = async () => {
-    return new Promise((resolve, reject) => {
-        const req = https.get(ckey.SERVER_URL, (res) => {
-            if (res.statusCode === 200) {
-                console.log('Server pinged successfully');
-                resolve({
-                    statusCode: 200,
-                    body: 'Server pinged successfully',
-                });
-            } else {
-                reject(
-                    new Error(`Server ping failed with status code: ${res.statusCode}`)
-                );
-            }
-        });
-
-        req.on('error', (error) => {
-            reject(
-                new Error(`Server pinged failed with error: ${error}`)
-            );
-        });
-
-        req.end();
-    });
-};
-
-pingServer();
-setInterval(pingServer, 1000 * 60 * 13);
